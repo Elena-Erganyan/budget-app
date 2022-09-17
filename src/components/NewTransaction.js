6758fb4844027f99eb3ae0e4bc59f9a9f50819e5
Elena Erganyan <elena-erganyan@rambler.ru>
2022-09-17 16:19:23 +0300

    Added fields for a new transaction

--- src/components/NewTransaction.js
+++ src/components/NewTransaction.js
@@ -3,6 +3,14 @@ import React from "react";
 const NewTransaction = () => {
   return <div>
     <h2>New transaction</h2>
+    <label for="title">Title</label>
+    <input id="title" type="text" />
+    <label for="amount">Amount</label>
+    <input id="amount" type="number" />
+    <label for="income">Income</label>
+    <input id="income" name="type" type="radio" value="income" />
+    <label for="expense">Expense</label>
+    <input id="expense" name="type" type="radio" value="expense" />
   </div>
 };
 
